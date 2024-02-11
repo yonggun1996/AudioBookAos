efe68e0c4cc5fe1695e762daabe95a00b7215e84
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2024-02-11 17:20:44 +0900

    Lazy Column 구현

--- app/src/main/java/com/example/audiobook/MainActivity.kt
+++ app/src/main/java/com/example/audiobook/MainActivity.kt
@@ -2,14 +2,27 @@
 
 package com.example.audiobook
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -20,10 +33,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.audiobook.model.MySubscribeModel
 import com.example.audiobook.ui.theme.AudioBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,19 +56,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TopAppBar()
+                    SetActivity()
                 }
             }
         }
     }
 }
 
-// 상단 헤더 구현
-// https://developer.android.com/jetpack/compose/components/app-bars?hl=ko
+// 콘텐츠 구현
+// TopAppBar : https://developer.android.com/jetpack/compose/components/app-bars?hl=ko
+// LazyColumn : https://makb.medium.com/listview-recyclerview-using-android-jetpack-compose-lazycolumn-ad905f52bf09
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun TopAppBar() {
+fun SetActivity() {
     Scaffold (
-        topBar = {
+        topBar = { // 헤더 구현
             val context = LocalContext.current
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
@@ -83,6 +105,55 @@ fun TopAppBar() {
             )
         }
     ) {
+        // LazyColumn 데이터 삽입
+        val contentList: MutableList<MySubscribeModel> = mutableListOf<MySubscribeModel>()
 
+        contentList.add(MySubscribeModel("나의 캐리어", "전영민", "부크크"))
+        contentList.add(MySubscribeModel("비전공자를 위한 이해할 수 있는 IT지식", "최원영", "티더블유아이지"))
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top, // 상단 헤더영역 아래로 배치하기 위한 로직
+        ) {
+            items(contentList) { model ->
+                ListItem(model = model)
+            }
+        }
+    }
+}
+
+/*
+ * 리스트 요소 setting
+ */
+@Composable
+fun ListItem(model: MySubscribeModel) {
+    Row() {
+        val paddingModifier = Modifier.padding(10.dp)
+        Card(elevation = CardDefaults.cardElevation(defaultElevation = 10.dp), modifier = paddingModifier) {
+            Row(
+                // 수평 폭 기기 크기에 맞게 세팅
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // url로 이미지를 가져와야 하는 경우
+                // https://developer.android.com/jetpack/compose/graphics/images/loading?hl=ko
+                Image(
+                    painter = painterResource(R.drawable.book_icon),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(5.dp)
+                )
+                Text(
+                    text = model.bookTitle,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black,
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/audiobook/model/MySubscribeModel.kt
@@ -0,0 +1,9 @@
+package com.example.audiobook.model
+
+/*
+ * 개인 관심 도서 목록 Model
+ * bookTitle : 책 제목
+ * writerName : 작가명
+ * publisher : 출판사
+ */
+data class MySubscribeModel (val bookTitle: String, val writerName: String, val publisher: String)
